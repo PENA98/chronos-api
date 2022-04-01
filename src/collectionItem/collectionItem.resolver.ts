84dd0ab841500cad7e9f90460fb5d9bc6c01ce07
PENA98 <maynorx5000@gmail.com>
2022-04-01 02:10:32 -0600

    Finished resolvers and service for both models

--- src/collectionItem/collectionItem.resolver.ts
+++ src/collectionItem/collectionItem.resolver.ts
@@ -1,13 +1,36 @@
-import { Resolver, Query } from '@nestjs/graphql';
-import { CollectionItem } from './collectionItem.schema';
+import { Resolver, Query, Mutation } from '@nestjs/graphql';
+import {
+  CollectionItem,
+  createCollectionItemInput,
+} from './collectionItem.schema';
 import { CollectionItemService } from './collectionItem.service';
 @Resolver()
 export class CollectionItemResolver {
+  constructor(private collectionItemService: CollectionItemService) {}
 
-    constructor(private collectionItemService: CollectionItemService) {}
+  @Query(() => [CollectionItem]) // <-- what the query will return
+  async collectionItems() {
+    // <-- query name
+    return this.collectionItemService.findAll(); // <-- resolve the query
+  }
 
-    @Query(() => [CollectionItem]) // <-- what the query will return
-    async collectionItems() { // <-- query name
-        return this.collectionItemService.findAll(); // <-- resolve the query
-    }
+  @Query(() => CollectionItem)
+  async collectionItem(id: string) {
+    return this.collectionItemService.findById(id);
+  }
+
+  @Mutation(() => CollectionItem)
+  async createCollectionItem(collectionItem: createCollectionItemInput) {
+    return this.collectionItemService.createCollectionItem(collectionItem);
+  }
+
+  @Mutation(() => CollectionItem)
+  async updateCollectionItem(collectionItem: createCollectionItemInput) {
+    return this.collectionItemService.updateCollectionItem(collectionItem);
+  }
+
+  @Mutation(() => CollectionItem)
+  async deleteCollectionItem(id: string) {
+    return this.collectionItemService.deleteCollectionItem(id);
+  }
 }
