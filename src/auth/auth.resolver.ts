20cf692a9077dca5f6e6431a353f574e4795171e
PENA98 <maynorx5000@gmail.com>
2022-04-01 20:37:32 -0600

    Added GqlAuthGuard

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LoginResponse } from './dto/login.response';
+import { LoginUserInput } from './dto/loginUser.input';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+    constructor(
+        private authService: AuthService
+    ) {}
+
+    @Mutation(() => LoginResponse)
+    async login(@Args('LoginUserInput') loginUserInput: LoginUserInput): Promise<LoginResponse> {
+        return this.authService.login(loginUserInput);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../users/user.service';
+import { LoginUserInput } from './dto/loginUser.input';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(loginUserInput: LoginUserInput): Promise<any> {
+    const user = await this.userService.findOne(loginUserInput.username);
+    const { password, ...result } = user;
+    return {
+      access_token: 'fake-token',
+      user: result,
+    };
+    return null;
+  }
+}
